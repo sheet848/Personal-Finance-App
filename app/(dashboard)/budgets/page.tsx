bcc3ea8c9482a95e594795f91e6faa7248d336e6
Sheetal Naik <sheetalnaik848@gmail.com>
2026-01-27 17:12:26 +0530

    transaction page and dashboard
    - transaction page -> filtering and sorting is left
    - dashboard overview released partly

---
+++ app/(dashboard)/budgets/page.tsx
@@ -0,0 +1,7 @@
+export default function Budgets() {
+  return (
+    <main className="flex min-h-[92vh] flex-col items-center justify-between p-6">
+      Budget
+    </main>
+  );
+}
---
+++ app/(dashboard)/pots/page.tsx
@@ -0,0 +1,7 @@
+export default function Pots() {
+  return (
+    <main className="flex min-h-[92vh] flex-col items-center justify-between p-6">
+      Pots
+    </main>
+  );
+}
