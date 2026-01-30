bfb63614ab7c1670ee3fbef2173738902811a8ce
Sheetal Naik <sheetalnaik848@gmail.com>
2026-01-30 17:46:28 +0530

    Delete app directory

--- app/(dashboard)/budgets/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Budgets() {
-  return (
-    <main className="flex min-h-[92vh] flex-col items-center justify-between p-6">
-      Budget
-    </main>
-  );
-}
--- app/(dashboard)/recurring-bills/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RecurringBills() {
-  return (
-    <main className="flex min-h-[92vh] flex-col items-center justify-between p-6">
-      Recurring Bills
-    </main>
-  );
-}
