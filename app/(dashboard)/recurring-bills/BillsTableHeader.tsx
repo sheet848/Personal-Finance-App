61e82bf05fa7b5c623b3f9a637ea09b35290285d
Sheetal Naik <sheetalnaik848@gmail.com>
2026-02-02 16:24:12 +0530

    Delete app directory

--- app/(dashboard)/recurring-bills/BillsTableHeader.tsx
+++
@@ -1,9 +0,0 @@
-export default function BillsTableHeader() {
-  return (
-    <div className="grid grid-cols-12 py-3 text-sm text-gray-500">
-      <div className="col-span-6">Bill Title</div>
-      <div className="col-span-3">Due Date</div>
-      <div className="col-span-3 text-right">Amount</div>
-    </div>
-  );
-}
--- app/(dashboard)/recurring-bills/TotalBillsCard.tsx
+++
@@ -1,8 +0,0 @@
-export default function TotalBillsCard() {
-    return (
-        <div className="bg-gray-900 text-white rounded-xl p-5">
-            <p className="text-sm text-gray-400">Total bills</p>
-            <p className="text-2xl font-bold mt-2">$1000001000011404.00</p>
-        </div>
-    );
-}   
